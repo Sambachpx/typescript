5e8e4fed222df75fa2811172d77cbb3c2608538a
Sambachpx <113524274+Sambachpx@users.noreply.github.com>
2024-11-08 06:54:45 +0100

    feat: exercice API response

--- src/exercises/4-generics/generic2.ts
+++ src/exercises/4-generics/generic2.ts
@@ -7,3 +7,37 @@ Include utility functions to handle both cases
 */
 
 // Write your solution here
+
+interface User {
+  id: number;
+  name: string;
+}
+
+type Success<T> = {
+  status: "success";
+  test: T;
+};
+
+type Error = {
+  // TODO: voir pour le bug
+  status: "erreur";
+  message: string;
+};
+
+type ApiResponse<T> = Success<T> | Error;
+
+function successResponse<T>(props: T): Success<T> {
+  return {
+    status: "success",
+    test: props,
+  };
+}
+
+function errorResponse(message: string): Error {
+  return {
+    status: "erreur",
+    message: message,
+  };
+}
+
+// TODO: faire le cas ou ca fonctionne ou non / réponse API
